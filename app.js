eb90103ba368e6d8dae86b5214fcda6df27b4a95
Vinicius Lazzaroto <vinilazza2016cris@gmail.com>
2022-07-27 11:27:04 -0300

    Adicionado numeração

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("OI TUDO BEM!?")
-console.log("OI TUDO BEM!?")
-console.log("OI TUDO BEM!?")
-console.log("OI TUDO BEM!?")
+console.log("OI TUDO BEM2!?")
+console.log("OI TUDO BEM3!?")
+console.log("OI TUDO BEM4!?")
